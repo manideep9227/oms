a10bd983d560425a5497e2f5fe48b39ce4ab1887
Anusuri Manideep <manideep9227@gmail.com>
2022-11-01 20:43:15 +0530

    Service discovery

---
+++ service-registration-and-discovery/src/main/java/com/example/serviceregistrationanddiscovery/ServiceRegistrationAndDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceregistrationanddiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegistrationAndDiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistrationAndDiscoveryApplication.class, args);
+	}
+
+}
