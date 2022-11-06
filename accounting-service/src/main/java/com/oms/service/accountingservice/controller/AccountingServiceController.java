906d5a190cfeb1b5db198f8e244bd5a6dae817ad
Anusuri Manideep <manideep9227@gmail.com>
2022-11-06 10:25:58 +0530

    Service discovery

--- accounting-service/src/main/java/com/oms/service/accountingservice/controller/AccountingServiceController.java
+++ accounting-service/src/main/java/com/oms/service/accountingservice/controller/AccountingServiceController.java
@@ -1,7 +1,11 @@
 package com.oms.service.accountingservice.controller;
 
 import com.oms.service.accountingservice.service.AccountingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,11 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/accounts")
 public class AccountingServiceController {
+    Logger LOGGER = LoggerFactory.getLogger(AccountingServiceController.class);
 
     @Autowired
     private AccountingService accountingServiceImpl;
     @GetMapping("/getOrders")
-    public @ResponseBody String getOrders(){
-        return accountingServiceImpl.getOrders();
+    public @ResponseBody ResponseEntity<String> getOrders(){
+        try{
+            return new ResponseEntity<>(accountingServiceImpl.getOrders(), HttpStatus.OK);
+        }catch (Exception e){
+            LOGGER.error("Exception occurred: {}",e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
+        }
+
     }
 }
--- order-service/src/main/java/com/oms/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/oms/example/orderservice/controller/OrderController.java
@@ -1,12 +1,18 @@
 package com.oms.example.orderservice.controller;
 
+import com.oms.example.orderservice.exception.SystemException;
 import com.oms.example.orderservice.model.FoodItem;
 import com.oms.example.orderservice.model.OrderDetails;
 import com.oms.example.orderservice.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 /**
  * RestController for creating OrderItems and Retrieving OrderItems based on orderId
@@ -15,6 +21,8 @@ import java.util.List;
 @RequestMapping("/oms")
 public class OrderController {
 
+    Logger LOGGER = LoggerFactory.getLogger(OrderController.class);
+
     @Autowired
     private OrderService orderItemService;
 
@@ -22,18 +30,37 @@ public class OrderController {
      * API to retrieve OrderItems based on @orderId
      */
     @GetMapping("/getOrderItemInfo/{orderId}")
-    public OrderDetails getOrderItems(@PathVariable("orderId") String orderId){
-        return orderItemService.getOrderItem(orderId);
+    public @ResponseBody ResponseEntity<Object> getOrderItems(@PathVariable("orderId") String orderId){
+        try{
+            LOGGER.info("Getting OrderItems");
+            return new ResponseEntity<>(orderItemService.getOrderItem(orderId), HttpStatus.OK);
+        }catch (SystemException e){
+            LOGGER.error("Exception occurred: {}",e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
+        }
+
     }
     /**
      * API to create new OrderItems
      */
     @PostMapping("/createNewOrderItem")
-    public OrderDetails createOrderItem(@RequestBody OrderDetails orderItem){
-        return orderItemService.createOrderItem(orderItem);
+    public @ResponseBody ResponseEntity<Object> createOrderItem(@RequestBody OrderDetails orderItem){
+        try{
+            return new ResponseEntity<>(orderItemService.createOrderItem(orderItem),HttpStatus.OK);
+        }catch (SystemException e){
+            LOGGER.error("Exception occurred: {}",e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
+        }
+
     }
     @GetMapping("/getAllOrders")
-    public @ResponseBody List<OrderDetails> getAllOrders(){
-        return orderItemService.getOrders();
+    public @ResponseBody ResponseEntity<Object> getAllOrders(){
+        try{
+            return new ResponseEntity<>(orderItemService.getOrders(),HttpStatus.OK);
+        }
+        catch (SystemException e){
+            LOGGER.error("Exception occurred: {}",e.getMessage());
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.FORBIDDEN);
+        }
     }
 }
